8c1f4cf3e525877bdc013f4fedfef06929fc315d
Mrittunjoy <mrittunjoy@bitmascot.com>
2025-12-19 16:11:08 +0600

    Inner design

--- wp-content/themes/notout/404.php
+++ wp-content/themes/notout/404.php
@@ -28,7 +28,7 @@ get_header();
 						<div class="page-content">
 							<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'notout' ); ?></p>
 
-							<?php get_search_form(); ?>
+							<!--<?php get_search_form(); ?>-->
 						</div><!-- .page-content -->
 					</section><!-- .error-404 -->
 
