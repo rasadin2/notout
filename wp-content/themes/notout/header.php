c5f0eb54425968ff6164c6ad65c65388343dfb69
rasadin <12105708+rasadin@users.noreply.github.com>
2025-12-29 21:28:47 +0600

    ELE

--- wp-content/themes/notout/header.php
+++ wp-content/themes/notout/header.php
@@ -20,9 +20,88 @@
 
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
+	<!-- Google Translate Script -->
+	<script type="text/javascript">
+		function googleTranslateElementInit() {
+			new google.translate.TranslateElement({
+				pageLanguage: 'bn',
+				includedLanguages: 'bn,en',
+				layout: google.translate.TranslateElement.InlineLayout.SIMPLE,
+				autoDisplay: false
+			}, 'google_translate_element');
+		}
+	</script>
+	<script type="text/javascript" src="https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
 
+	<style>
+		/* Hide Google Translate default widget */
+		#google_translate_element {
+			display: none;
+		}
+
+		/* Hide Google Translate toolbar */
+		.goog-te-banner-frame.skiptranslate {
+			display: none !important;
+		}
+
+		body {
+			top: 0px !important;
+		}
+
+		.goog-te-gadget {
+			display: none !important;
+		}
+
+		/* Optional: Style for language links */
+		.lang-switcher a {
+			cursor: pointer;
+			text-decoration: none;
+		}
+
+		.lang-switcher a:hover {
+			text-decoration: underline;
+		}
+	</style>
 
 	<?php wp_head(); ?>
+
+	<script type="text/javascript">
+		function setCookie(key, value, expiry) {
+			var expires = new Date();
+			expires.setTime(expires.getTime() + (expiry * 24 * 60 * 60 * 1000));
+			document.cookie = key + '=' + value + ';expires=' + expires.toUTCString() + ';path=/';
+		}
+
+		function getCookie(key) {
+			var keyValue = document.cookie.match('(^|;) ?' + key + '=([^;]*)(;|$)');
+			return keyValue ? keyValue[2] : null;
+		}
+
+		function translateToEnglish() {
+			setCookie('googtrans', '/bn/en', 1);
+			setCookie('googtrans', '/bn/en', 1);
+			window.location.reload();
+		}
+
+		function translateToBangla() {
+			setCookie('googtrans', '/bn/bn', 1);
+			setCookie('googtrans', '/bn/bn', 1);
+			window.location.reload();
+		}
+
+		// Wait for Google Translate to load
+		function checkGoogleTranslate() {
+			if (typeof google !== 'undefined' && google.translate) {
+				console.log('Google Translate loaded successfully');
+			} else {
+				setTimeout(checkGoogleTranslate, 100);
+			}
+		}
+
+		window.addEventListener('load', function() {
+			checkGoogleTranslate();
+		});
+	</script>
 </head>
 
 <body <?php body_class(); ?>>
--- wp-content/themes/notout/template-parts/menu/content-inline-menu.php
+++ wp-content/themes/notout/template-parts/menu/content-inline-menu.php
@@ -11,8 +11,12 @@
    <div class="right-list">
    <ul>
 	     <li><a href="#"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 36 36"><script xmlns="" id="eppiocemhmnlbhjplcgkofciiegomcon"/><script xmlns=""/><script xmlns=""/><path fill="#31373D" d="M34.06 26.407l-3.496-3.496c-1.93-1.93-5.06-1.93-6.989 0-.719.718-1.167 1.603-1.351 2.528-5.765-1.078-11.372-6.662-11.721-11.653.947-.176 1.854-.627 2.586-1.36 1.93-1.93 1.93-5.06 0-6.99L9.594 1.94c-1.93-1.93-5.06-1.93-6.99 0-10.486 10.486 20.97 41.942 31.456 31.456 1.929-1.929 1.929-5.059 0-6.989z"/></svg> ২৪/৭ সাপোর্ট</a></li>
-	     <li><a herf="#">বাংলা</a> <a herf="#">| EN</a></li>
-		 
+	     <li class="lang-switcher">
+			<div id="google_translate_element"></div>
+			<a href="#" onclick="translateToBangla(); return false;">বাংলা</a>
+			<a href="#" onclick="translateToEnglish(); return false;">| EN</a>
+		 </li>
+
 	  </ul>
    </div>
    </div>
